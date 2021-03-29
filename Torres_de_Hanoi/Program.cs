106a024998b46943fb1043156efe9585981d1ec1
Alberto <albertitovalls97@gmail.com>
2021-03-29 17:58:47 +0200

    cambios programa

--- Torres_de_Hanoi/Program.cs
+++ Torres_de_Hanoi/Program.cs
@@ -10,43 +10,43 @@ namespace Torres_de_Hanoi
     {
         static void Main(string[] args)
         {
+            //Iniciamos los discos según su tamaño
+            Disco discoPequenyo = new Disco(1);//disco pequeño
 
-            // pilas
-            Pila pilaInicial = new Pila();
+            Disco discoMediano = new Disco(2);//disco mediano
 
-            Pila pilaAux = new Pila();
-
-            Pila pilaFinal = new Pila();
-
-            //discos 
-            Disco discoPequenyo = new Disco(1);
-
-            Disco discoMediano = new Disco(2);
-
-            Disco discoGrande = new Disco(3);
+            Disco discoGrande = new Disco(3);//disco grande
 
 
+            // Iniciamos las pilas según su posición
+            Pila pilaInicial = new Pila();//primera pila
+
+            Pila pilaAux = new Pila();//segunda pila
+
+            Pila pilaFinal = new Pila();//tercera pila
+
+           
             //Insertamos los discos en las pilas
-            pilaInicial.push(discoGrande);
+            pilaInicial.push(discoGrande);//ponemos disco grande en primera pila
 
-            pilaInicial.push(discoMediano);
+            pilaInicial.push(discoMediano);//ponemos disco mediano en primera pila
 
-            pilaInicial.push(discoPequenyo);
+            pilaInicial.push(discoPequenyo);//ponemos disco pequeño en primera pila
 
 
             //Entramos en Hanoi y usamos sus metodos
-            Hanoi objetoHanoi = new Hanoi();
+            Hanoi objetoHanoi = new Hanoi();//iniciamos Hanoi
 
-            int resultado = objetoHanoi.iterativo(3, pilaInicial, pilaFinal, pilaAux);
+            int resultado = objetoHanoi.iterativo(3, pilaInicial, pilaFinal, pilaAux);//definimos el resultado iterativo
 
-            Console.WriteLine("\nNumero de movimientos totales= " + resultado);
-            if (resultado == 7)
+            Console.WriteLine("\nNumero de movimientos totales= " + resultado);//imprimimos por pantalla el numero de movimientos
+            if (resultado == 7)//si el resultado es 7
             {
 
-                Console.WriteLine("\nEl programa va bien");
+                Console.WriteLine("\nEl programa va bien");//para comprobar si el programa funciona correctamente
             }
 
-            Console.ReadKey();
+            Console.ReadKey();//termina el programa
         }
     }
 }
