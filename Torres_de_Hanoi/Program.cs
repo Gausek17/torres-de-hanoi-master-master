483a0b9a6caf28a2b0c8563e786aa6c6910e9086
Alberto <albertitovalls97@gmail.com>
2021-03-29 17:52:52 +0200

    Programa Hanoi

--- Torres_de_Hanoi/Program.cs
+++ Torres_de_Hanoi/Program.cs
@@ -11,8 +11,41 @@ namespace Torres_de_Hanoi
         static void Main(string[] args)
         {
 
-            // Keep the console window open in debug mode.
-            Console.WriteLine("Press any key to exit.");
+            // pilas
+            Pila pilaInicial = new Pila();
+
+            Pila pilaAux = new Pila();
+
+            Pila pilaFinal = new Pila();
+
+            //discos 
+            Disco discoPequenyo = new Disco(1);
+
+            Disco discoMediano = new Disco(2);
+
+            Disco discoGrande = new Disco(3);
+
+
+            //Insertamos los discos en las pilas
+            pilaInicial.push(discoGrande);
+
+            pilaInicial.push(discoMediano);
+
+            pilaInicial.push(discoPequenyo);
+
+
+            //Entramos en Hanoi y usamos sus metodos
+            Hanoi objetoHanoi = new Hanoi();
+
+            int resultado = objetoHanoi.iterativo(3, pilaInicial, pilaFinal, pilaAux);
+
+            Console.WriteLine("\nNumero de movimientos totales= " + resultado);
+            if (resultado == 7)
+            {
+
+                Console.WriteLine("\nEl programa va bien");
+            }
+
             Console.ReadKey();
         }
     }
