081e09bcbe441544f92c8f89a760d07a5b735236
Alberto <albertitovalls97@gmail.com>
2021-03-29 17:51:15 +0200

    hanoi

--- Torres_de_Hanoi/Hanoi.cs
+++ Torres_de_Hanoi/Hanoi.cs
@@ -7,27 +7,146 @@ using System.Threading.Tasks;
 
 namespace Torres_de_Hanoi
 {
+
     class Hanoi
     {
-        /*TODO: Implementar métodos*/
+
+
         public void mover_disco(Pila a, Pila b)
         {
-            
-        }
-
-        public int iterativo(int n, Pila ini, Pila aux, Pila fin)
-        {
-            if (n == 1)
+            //Si hay una pila vacia
+            if (a.isEmpty() && b.Top != 0)
             {
-                cout << "Mover disco desde" << ini << "a" << aux << endl;
+                Disco discoMovido = b.pop();
+                a.push(discoMovido);
+
+
+                Console.WriteLine("Movimiento de disco con valor: " + discoMovido.Valor);
+
+
+
+            }
+            else if (!a.isEmpty() && b.isEmpty())
+            {
+                Disco discoMovido = a.pop();
+                b.push(discoMovido);
+
+                Console.WriteLine("Movimiento de disco con valor: " + discoMovido.Valor);
+
             }
             else
             {
-                iterativo(n - 1, ini, fin, aux);
-                cout << "Mover disco desde" << ini << "a" << aux << endl;
-                iterativo(n - 1, fin, aux, ini);
+                //dos pilas llenas
+                int inicio = a.Top;
+                int final = b.Top;
+                if (inicio > final)
+                {
+                    //B->A
+                    Disco discoMovido = b.pop();
+                    a.push(discoMovido);
+
+                    Console.WriteLine("Movimiento de disco con valor: " + discoMovido.Valor);
+                }
+                else
+                {
+                    //A->B
+                    Disco discoMovido = a.pop();
+                    b.push(discoMovido);
+
+                    Console.WriteLine("Movimiento de disco con valor: " + discoMovido.Valor);
+                }
             }
+
+
+
         }
+
+
+        public int iterativo(int n, Pila ini, Pila fin, Pila aux)
+        {
+            int numero_movimientos = 0;
+            bool noSolucionado = true;
+
+            if (n % 2 != 0)
+            {
+                while (noSolucionado)
+                {
+
+
+                    mover_disco(ini, fin);
+                    numero_movimientos++;
+
+                    if (fin.Size == n)
+                    {
+                        noSolucionado = false;
+                        break;
+                    }
+
+                    mover_disco(ini, aux);
+                    numero_movimientos++;
+                    if (fin.Size == n)
+                    {
+                        noSolucionado = false;
+                        break;
+                    }
+
+                    mover_disco(aux, fin);
+                    numero_movimientos++;
+                    if (fin.Size == n)
+                    {
+                        noSolucionado = false;
+                        break;
+                    }
+
+
+                }
+
+            }
+
+
+            if (n % 2 == 0)
+            {
+                while (noSolucionado)
+                {
+
+                    mover_disco(ini, fin);
+                    numero_movimientos++;
+
+                    if (fin.Size == n)
+                    {
+                        noSolucionado = false;
+                        break;
+                    }
+
+                    mover_disco(ini, aux);
+                    numero_movimientos++;
+
+                    if (fin.Size == n)
+                    {
+                        noSolucionado = false;
+                        break;
+                    }
+
+                    mover_disco(aux, fin);
+                    numero_movimientos++;
+
+                    if (fin.Size == n)
+                    {
+                        noSolucionado = false;
+                        break;
+                    }
+
+
+                }
+
+            }
+
+
+
+            return numero_movimientos;
+        }
+
 
     }
 }
+    
