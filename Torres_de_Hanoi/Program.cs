943cf6a2e31019b0bda775084bd27226399bd994
Alberto <albertitovalls97@gmail.com>
2021-04-24 15:25:27 +0200

    Cambios programa

--- Torres_de_Hanoi/Program.cs
+++ Torres_de_Hanoi/Program.cs
@@ -11,27 +11,31 @@ namespace Torres_de_Hanoi
         //inicio Main
         static void Main(string[] args)
         {
-
-            bool follow = true;
+            //por si el usuario quiere seguir con el programa
+            bool continuar = true;
 
             Console.WriteLine("Inicio Programa TORRES DE HANOI");
 
-            while (follow)
+            while (continuar)
             {
                 // Pedimos al usuario el numero de discos
                 Console.WriteLine("Introduzca numero de piezas: ");
                 
+                
+                
                 //convertimos m en un numero entero
                 int n = Convert.ToInt32(Console.ReadLine());
 
-                // crear las 3 pilas y el objeto hanoi
+                // iniciamos las tres pilas
                 Pila p1 = new Pila("INI");
+                
                 Pila p2 = new Pila("FIN");
+                
                 Pila p3 = new Pila("AUX");
 
                 Hanoi h = new Hanoi();
 
-                // añadir los n discos a la pila ini
+                //añadimos el numero de discos a la pila inicial
                 for (int i = n; i >= 1; i--)
                 {
                     p1.push(new Disco(i));
@@ -41,17 +45,28 @@ namespace Torres_de_Hanoi
                 Console.WriteLine(p3.ToString());
                 Console.WriteLine(p2.ToString());
 
-                Console.WriteLine("Se completo en: " + h.iterativo(p1.Elementos.Count, p1, p2, p3) + " movimientos");
+                //METODO ITERATIVO
+                Console.WriteLine("El resultado final fue de: " + h.iterativo(p1.Elementos.Count, p1, p2, p3) + " movimientos  (Metodo Iterativo)");
 
-                //Console.WriteLine("Se completo en: " + h.recursivo(p1.Elementos.Count, p1, p2, p3) + " movimientos");
 
-                Console.WriteLine("El número mínimo de movimientos para " + n + " piezas es: " + (Math.Pow(2, n) - 1));
+                //METODO RECURSIVO
+                Console.WriteLine("El resultado final fue de: " + h.recursivo(p1.Elementos.Count, p1, p2, p3) + " movimientos (Metodo Recursivo)");
 
-                Console.WriteLine("Pulsa 'q' para salir o cualquier otra para repetir");
-                char c = Console.ReadLine()[0];
-                if (c.Equals('q'))
+                
+                //calculamos matemáticamente el resultado(dos elevado al numero de piezas menos uno
+                Console.WriteLine("La solución para " + n + " piezas es: " + (Math.Pow(2, n) - 1));
+
+
+                //continuar con el programa
+                Console.WriteLine("Pulse '0' para salir o si quiere seguir con el programa cualquier otra letra");
+                
+                char f = Console.ReadLine()[0];
+               
+                //si pulsa 0
+                if (f.Equals('0'))
                 {
-                    follow = false;
+                    //el programa ya no sigue
+                    continuar = false;
                 }
             }
 
